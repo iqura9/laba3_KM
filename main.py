557490fd70c7629d452ccda8a386ce6ce97303f4
Саша Корнієнко <iqura@Sasas-MacBook-Pro.local>
2023-12-02 01:13:43 +0200

    second commit

--- main.py
+++ main.py
@@ -2,20 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def heun_method(f, y0, t0, tn, h):
-    """
-    Solves a first-order ODE using Heun's method.
-
-    Parameters:
-        f: Function representing the ODE dy/dt = f(t, y)
-        y0: Initial value of the dependent variable
-        t0: Initial time
-        tn: Final time
-        h: Step size
-
-    Returns:
-        t_values: Array of time values
-        y_values: Array of corresponding solution values
-    """
     num_steps = int((tn - t0) / h) + 1
     t_values = np.linspace(t0, tn, num_steps)
     y_values = np.zeros(num_steps)
@@ -27,6 +13,22 @@ def heun_method(f, y0, t0, tn, h):
         y_values[i] = y_values[i - 1] + 0.5 * h * (k1 + k2)
 
     return t_values, y_values
+
+def milne_simpson_method(f, y0, t0, tn, h):
+    num_steps = int((tn - t0) / h) + 1
+    t_values = np.linspace(t0, tn, num_steps)
+    y_values = np.zeros(num_steps)
+    y_values[0] = y0
+
+    for i in range(0, num_steps - 3, 2):
+        k1 = f(t_values[i], y_values[i])
+        k2 = f(t_values[i + 1], y_values[i + 1])
+        k3 = f(t_values[i + 2], y_values[i + 2])
+
+        y_values[i + 2] = y_values[i] + (h / 3) * (k1 + 4 * k2 + k3)
+    
+    return t_values, y_values
+
 
 # Given ODE: y' = -a*b*t*y^2
 def example_ode(t, y, a, b):
@@ -40,10 +42,15 @@ t0 = a
 tn = a + 3
 h = 0.1
 
-t_values, y_values = heun_method(lambda t, y: example_ode(t, y, -a, b), b, t0, tn, h)
+# Heun's Method
+t_values_heun, y_values_heun = heun_method(lambda t, y: example_ode(t, y, -a, b), b, t0, tn, h)
+
+# Milne-Simpson Method
+t_values_ms, y_values_ms = milne_simpson_method(lambda t, y: example_ode(t, y, -a, b), b, t0, tn, h)
 
 # Plot the results
-plt.plot(t_values, y_values, label='Heun\'s Method')
+plt.plot(t_values_heun, y_values_heun, label='Heun\'s Method')
+plt.plot(t_values_ms, y_values_ms, label='Milne-Simpson Method')
 plt.xlabel('Time')
 plt.ylabel('Solution y(t)')
 plt.legend()
